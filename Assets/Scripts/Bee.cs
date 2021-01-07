4047d5432078d0c6fec332f5005f3d25d0d660d4
BrendanToolan <btoolan73@gmail.com>
2021-01-07 16:19:09 +0000

    movement added to bee

---
+++ Assets/Scripts/Bee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+
+    Rigidbody2D beeRB;
+    BoxCollider2D beeCollider;
+
+    public float beeSpeed = 3f;
+    private float energyRate = 0.3f;
+    private Vector2 movementDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beeRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beeRB.velocity = new Vector2(beeSpeed, 0f);   
+    }
+}
---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    Rigidbody2D birdRB;
+    BoxCollider2D birdCollider;
+
+    public float speed = 4f;
+    private float energyRate = 0.3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
